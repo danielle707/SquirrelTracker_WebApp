5df3485e913a9a6b86c58aa18193750ef6b9709a
Zixuanqi19 <you@example.com>
2019-12-08 00:20:05 -0500

    add collected-statistics

--- SquirrelTracker_WebApp/SquirrelTracker_WebApp/settings.py
+++ SquirrelTracker_WebApp/SquirrelTracker_WebApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'yc&rfs((*1is8#opdc=)cgu9(2ey$2f38(mdkoqz9hn=b)6$3_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'collected-static'
